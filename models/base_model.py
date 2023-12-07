3ddf9fce62dac604a84ce05523ad954dbe749f7c
The-alpha-male <ronnymunene@outlook.com>
2023-12-07 19:48:17 +0300

    Updated README

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""This script is the base model that defines all common attributes/methods for other classes"""
+"""This script is the base model that defines all common
+attributes/methods for other classes"""
 
 import uuid
 from datetime import datetime
 import models
 
 class BaseModel:
-    """This class is the base model that defines all common attributes/methods for other classes."""
+
+    """This class is the base model that defines
+    all common attributes/methods for other classes."""
+
     def __init__(self, *args, **kwargs):
         """This is the initialization method"""
         if kwargs:
