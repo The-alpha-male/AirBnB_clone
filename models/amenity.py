677ee48df0e323aa826ed531e4f74d3a8b25783b
The-alpha-male <ronnymunene@outlook.com>
2023-12-08 17:10:05 +0300

    created amenities class

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""model class amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+
+    """child class of BaseModel
+    """
+    name = ""
